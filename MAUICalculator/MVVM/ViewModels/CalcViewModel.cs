2196416596fd2bebf73e52645b743eea6026b8fb
rushi-smb <rushi@smbios.co.in>
2023-02-28 23:30:08 +0530

    Additional Command - Backspace and AC

--- MAUICalculator/MVVM/ViewModels/CalcViewModel.cs
+++ MAUICalculator/MVVM/ViewModels/CalcViewModel.cs
@@ -15,5 +15,19 @@ namespace MAUICalculator.MVVM.ViewModels
         public string Result { get; set; } = "0";
         public ICommand OperationCommand =>
             new Command((number) => { Formula += number; });
+        public ICommand ResetCommand =>
+            new Command(() =>
+            {
+                Result = "0";
+                Formula = "";
+            });
+        public ICommand BackSpaceCommand =>
+            new Command(() =>
+            {
+                if (Formula.Length > 0)
+                {
+                    Formula = Formula.Substring(0, Formula.Length - 1);
+                }
+            });
     }
 }
